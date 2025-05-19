15f06591561f0ff84eda6a89ef72d26f38c86d73
Israfil Guseinov <israfil.guseynov.98@gmail.com>
2025-05-18 19:04:08 -0700

    Build the new issue page

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,12 +1,15 @@
 'use client';
 
 import { Button } from '@radix-ui/themes';
+import Link from 'next/link';
 
 const IssuesPage = () => {
   return (
     <div>
       IssuesPage
-      <Button>New Issue</Button>
+      <Button>
+        <Link href='/issues/new'>New Issue</Link>
+      </Button>
     </div>
   );
 };
