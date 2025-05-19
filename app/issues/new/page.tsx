43dc0de2cc7a24ede437064a96d4ab770d7ecc30
Israfil Guseinov <israfil.guseynov.98@gmail.com>
2025-05-18 21:31:45 -0700

    Extract the ErrorMessage component

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,8 +1,9 @@
 'use client';
 
+import ErrorMessage from '@/app/components/ErrorMessage';
 import { createIssueSchema } from '@/app/validationSchemas';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { Button, Callout, Text, TextField } from '@radix-ui/themes';
+import { Button, Callout, TextField } from '@radix-ui/themes';
 import axios from 'axios';
 import 'easymde/dist/easymde.min.css';
 import { useRouter } from 'next/navigation';
@@ -54,13 +55,7 @@ const NewIssuePage = () => {
           placeholder='Title'
           {...register('title')}
         />
-        {errors.title && (
-          <Text
-            color='red'
-            as='p'>
-            {errors.title.message}
-          </Text>
-        )}
+        <ErrorMessage>{errors.title?.message}</ErrorMessage>
         <Controller
           name='description'
           control={control}
@@ -71,13 +66,7 @@ const NewIssuePage = () => {
             />
           )}
         />
-        {errors.description && (
-          <Text
-            color='red'
-            as='p'>
-            {errors.description.message}
-          </Text>
-        )}
+        <ErrorMessage>{errors.description?.message}</ErrorMessage>
         <Button>Submit New Issue</Button>
       </form>
     </div>
