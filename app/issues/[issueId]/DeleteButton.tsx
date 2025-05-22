dbcde168bf0b9f793f1ca895590f8a47d30b66d0
Israfil Guseinov <israfil.guseynov.98@gmail.com>
2025-05-21 18:07:47 -0700

    Show a delete confirmation dialog alert

--- app/issues/[issueId]/DeleteButton.tsx
+++ app/issues/[issueId]/DeleteButton.tsx
@@ -1,14 +1,43 @@
 'use client';
 
 import { TrashIcon } from '@radix-ui/react-icons';
-import { Button } from '@radix-ui/themes';
+import { AlertDialog, Button, Flex } from '@radix-ui/themes';
 
 const DeleteButton = ({ issueId }: { issueId: number }) => {
   return (
-    <Button color='red'>
-      <TrashIcon />
-      Delete Issue
-    </Button>
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color='red'>
+          <TrashIcon />
+          Delete Issue
+        </Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content maxWidth='500px'>
+        <AlertDialog.Title>Delete Issue</AlertDialog.Title>
+        <AlertDialog.Description size='3'>
+          Are you sure you want to delete this issue? This action is permanent
+          and cannot be undone.
+        </AlertDialog.Description>
+        <Flex
+          mt='3'
+          gap='3'
+          justify='end'>
+          <AlertDialog.Cancel>
+            <Button
+              variant='soft'
+              color='gray'>
+              Cancel
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button color='red'>
+              <TrashIcon />
+              Delete Issue
+            </Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
   );
 };
 
