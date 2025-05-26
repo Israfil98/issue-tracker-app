984d1495873ad0253e15a2bdb964a2e9284d72fd
Israfil Guseinov <israfil.guseynov.98@gmail.com>
2025-05-25 20:43:00 -0700

    Setup react query

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Inter } from 'next/font/google';
 import AuthProvider from './auth/Provider';
 import './globals.css';
 import NavBar from './NavBar';
+import QueryClientProvider from './QueryClientProvider';
 import './theme-config.css';
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
@@ -22,16 +23,18 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={inter.variable}>
-        <AuthProvider>
-          <Theme
-            accentColor='violet'
-            appearance='light'>
-            <NavBar />
-            <main className='p-5'>
-              <Container>{children}</Container>
-            </main>
-          </Theme>
-        </AuthProvider>
+        <QueryClientProvider>
+          <AuthProvider>
+            <Theme
+              accentColor='violet'
+              appearance='light'>
+              <NavBar />
+              <main className='p-5'>
+                <Container>{children}</Container>
+              </main>
+            </Theme>
+          </AuthProvider>
+        </QueryClientProvider>
       </body>
     </html>
   );
