83f069be9c1d55091e3ec24a3fa1f3815df53728
Israfil Guseinov <israfil.guseynov.98@gmail.com>
2025-05-26 17:23:06 -0700

    Show toast notifications

--- app/issues/[issueId]/AssigneeSelect.tsx
+++ app/issues/[issueId]/AssigneeSelect.tsx
@@ -5,6 +5,7 @@ import { Issue, User } from '@prisma/client';
 import { Avatar, Flex, Select, Text } from '@radix-ui/themes';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
+import toast, { Toaster } from 'react-hot-toast';
 
 const AssigneeSelect = ({ issue }: { issue: Issue }) => {
   const {
@@ -23,48 +24,54 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
   if (error) return null;
 
   return (
-    <Select.Root
-      defaultValue={issue.assignedToUserId || 'unassign'}
-      onValueChange={(userId) => {
-        axios.patch('/api/issues/' + issue.id, {
-          assignedToUserId: userId === 'unassign' ? null : userId,
-        });
-      }}>
-      <Select.Trigger
-        placeholder='Assign...'
-        variant='soft'
-        radius='large'
-      />
-      <Select.Content>
-        <Select.Group>
-          <Select.Label>Suggestions</Select.Label>
-          <Select.Item value='unassign'>Unassign</Select.Item>
-          {users?.map((user) => {
-            return (
-              <Select.Item
-                key={user.id}
-                value={user.id}>
-                <Flex justify='between'>
-                  <Flex
-                    align='center'
-                    gap='3'>
-                    {user.image && (
-                      <Avatar
-                        size='1'
-                        radius='full'
-                        src={user.image}
-                        fallback={user.name!}
-                      />
-                    )}
-                    <Text>{user.name}</Text>
+    <>
+      <Select.Root
+        defaultValue={issue.assignedToUserId || 'unassign'}
+        onValueChange={(userId) => {
+          axios
+            .patch('/api/issues/' + issue.id, {
+              assignedToUserId: userId === 'unassign' ? null : userId,
+            })
+            .then(() => toast.success('Successfully assigned!'))
+            .catch(() => toast.error('Changes could not be saved.'));
+        }}>
+        <Select.Trigger
+          placeholder='Assign...'
+          variant='soft'
+          radius='large'
+        />
+        <Select.Content>
+          <Select.Group>
+            <Select.Label>Suggestions</Select.Label>
+            <Select.Item value='unassign'>Unassigned</Select.Item>
+            {users?.map((user) => {
+              return (
+                <Select.Item
+                  key={user.id}
+                  value={user.id}>
+                  <Flex justify='between'>
+                    <Flex
+                      align='center'
+                      gap='3'>
+                      {user.image && (
+                        <Avatar
+                          size='1'
+                          radius='full'
+                          src={user.image}
+                          fallback={user.name!}
+                        />
+                      )}
+                      <Text>{user.name}</Text>
+                    </Flex>
                   </Flex>
-                </Flex>
-              </Select.Item>
-            );
-          })}
-        </Select.Group>
-      </Select.Content>
-    </Select.Root>
+                </Select.Item>
+              );
+            })}
+          </Select.Group>
+        </Select.Content>
+      </Select.Root>
+      <Toaster />
+    </>
   );
 };
 
