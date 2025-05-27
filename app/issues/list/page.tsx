6a8103f79bb24724581d28b2e43562b2e303bf9f
Israfil Guseinov <israfil.guseynov.98@gmail.com>
2025-05-26 20:01:54 -0700

    Sort issues order by columns

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -1,29 +1,46 @@
 import autOptions from '@/app/auth/authOptions';
 import { prisma } from '@/prisma/client';
-import { Status } from '@prisma/client';
-import { Table } from '@radix-ui/themes';
+import { Issue, Status } from '@prisma/client';
+import { ArrowUpIcon } from '@radix-ui/react-icons';
+import { Flex, Table, Text } from '@radix-ui/themes';
 import { getServerSession } from 'next-auth';
-import Link from 'next/link';
+import { default as Link, default as NextLink } from 'next/link';
 import IssueStatusBadge from '../../components/IssueStatusBadge';
 import IssueToolbar from './IssueToolbar';
 
 interface Props {
-  searchParams: Promise<{ status: Status }>;
+  searchParams: Promise<{ status: Status; orderBy: keyof Issue }>;
+}
+
+interface TableColumn {
+  label: string;
+  value: keyof Issue;
+  className?: string;
 }
 
 const IssuesPage = async ({ searchParams }: Props) => {
+  const tableColumns: TableColumn[] = [
+    { label: 'Issue', value: 'title' },
+    { label: 'Status', value: 'status', className: 'hidden md:table-cell' },
+    { label: 'Created', value: 'createdAt', className: 'hidden md:table-cell' },
+  ];
+
   const session = await getServerSession(autOptions);
 
   const status = (await searchParams).status;
-
   const statuses = Object.values(Status);
-
   const currentFilterStatus = statuses.includes(status) ? status : undefined;
+
+  const orderBy = (await searchParams).orderBy;
+  const currentOrderBy = tableColumns.map((col) => col.value).includes(orderBy)
+    ? { [orderBy]: 'asc' }
+    : undefined;
 
   const issues = await prisma.issue.findMany({
     where: {
       status: currentFilterStatus,
     },
+    orderBy: currentOrderBy,
   });
 
   return (
@@ -33,13 +50,25 @@ const IssuesPage = async ({ searchParams }: Props) => {
       <Table.Root variant='surface'>
         <Table.Header>
           <Table.Row>
-            <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className='hidden md:table-cell'>
-              Status
-            </Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className='hidden md:table-cell'>
-              Created
-            </Table.ColumnHeaderCell>
+            {tableColumns.map(async (col) => {
+              return (
+                <Table.ColumnHeaderCell key={col.value}>
+                  <NextLink
+                    href={{
+                      query: { ...(await searchParams), orderBy: col.value },
+                    }}>
+                    <Flex
+                      align='center'
+                      gap='1'>
+                      <Text>{col.label}</Text>
+                      {col.value === (await searchParams).orderBy && (
+                        <ArrowUpIcon />
+                      )}
+                    </Flex>
+                  </NextLink>
+                </Table.ColumnHeaderCell>
+              );
+            })}
           </Table.Row>
         </Table.Header>
 
