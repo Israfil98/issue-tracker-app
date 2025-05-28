3a3a38fdbb6643d7daea64e781dbc8606d3840b7
Israfil Guseinov <israfil.guseynov.98@gmail.com>
2025-05-27 19:45:01 -0700

    Build the LatestIssues component

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,5 @@
-import Pagination from './components/Pagination';
+import LatestIssues from './LatestIssues';
 
-export default async function Home({
-  searchParams,
-}: {
-  searchParams: Promise<{ page: string }>;
-}) {
-  const currentPage = parseInt((await searchParams).page);
-
-  return (
-    <Pagination
-      itemCount={100}
-      pageSize={10}
-      currentPage={currentPage}
-    />
-  );
+export default async function Home() {
+  return <LatestIssues />;
 }
